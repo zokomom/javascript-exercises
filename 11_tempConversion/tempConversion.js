29da041e7ea835cdca0ef39059c18255c4bdd021
Atharv <172103387+zokomom@users.noreply.github.com>
2025-06-07 02:56:47 +0530

    Coded Solution and Tested

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Number(((fahr-32)*(5/9)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  return Number(((celc*9/5)+32).toFixed(1))
 };
 
 // Do not edit below this line
--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(val1,val2) {
+	return val1+val2
 };
 
-const subtract = function() {
-	
+const subtract = function(val1,val2) {
+	return val1-val2
 };
 
-const sum = function() {
-	
+const sum = function(val) {
+	return val.reduce((acc,curr)=>acc+curr,0)
 };
 
-const multiply = function() {
-
+const multiply = function(val) {
+  return val.reduce((acc,curr)=>acc*curr,1)
 };
 
-const power = function() {
-	
+const power = function(val1,val2) {
+	return val1**val2
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+  let val1 = val
+  let factor = 1;
+  for(let i = 1; i<=val;i++){
+    factor=factor*i
+  }
+  return factor
 };
 
 // Do not edit below this line
