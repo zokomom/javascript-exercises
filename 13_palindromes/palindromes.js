f1dc22beec667ccd9fa3e82c81f0a50360439df7
Atharv <172103387+zokomom@users.noreply.github.com>
2025-06-07 14:25:05 +0530

    solved 13_palidrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str=str.toLowerCase()
+    let str1 = '';
+    let punctuations = ['.', ',', '!', '?', ':', ';', '-', '_', '"', "'", '(', ')',' '];
+    let cleanedstr = ''
+    for(let i in str){
+        if(punctuations.includes(str[i])){
+           
+            continue
+        }
+        cleanedstr += str[i];
+        str1 = str[i]+str1
+    }
+    
+    return cleanedstr == str1
 };
 
 // Do not edit below this line
